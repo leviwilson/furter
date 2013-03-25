224d841da3f25e08c53db1454361cae7f91bf122
Levi Wilson <levi@leviwilson.com>
2013-03-25 09:14:18 -0400

    RubyMine suggestions about single quotes

--- spec/lib/furter/accessors/text_spec.rb
+++ spec/lib/furter/accessors/text_spec.rb
@@ -4,22 +4,22 @@ describe Furter::Accessors::Text do
   let(:placeholder) { Furter::Accessors::Text.new(:placeholder => 'Placeholder Text') }
   let(:label) { Furter::Accessors::Text.new(:label => 'accessibilityLabel') }
 
-  context "locating text fields" do
-    it "should work by placeholder" do
+  context 'locating text fields' do
+    it 'should work by placeholder' do
       placeholder.send(:selector).should eq("textField placeholder:'Placeholder Text'")
     end
 
-    it "should work by accessibility label" do
+    it 'should work by accessibility label' do
       label.send(:selector).should eq("textField marked:'accessibilityLabel'")
     end
   end
 
-  it "can get text" do
+  it 'can get text' do
     placeholder.should_receive(:frankly_map).with(anything, 'text').and_return(['expected text'])
     placeholder.get_text.should eq('expected text')
   end
 
-  it "can set text" do
+  it 'can set text' do
     placeholder.should_receive(:frankly_map).with(anything, 'becomeFirstResponder').ordered
     placeholder.should_receive(:frankly_map).with(anything, 'setText:', 'the new text').ordered
     placeholder.should_receive(:frankly_map).with(anything, 'endEditing:', true).ordered
@@ -27,12 +27,12 @@ describe Furter::Accessors::Text do
     placeholder.set_text 'the new text'
   end
 
-  it "knows if text exists anywhere" do
+  it 'knows if text exists anywhere' do
     placeholder.should_receive(:element_exists).with("view marked:'to find anywhere'")
     placeholder.has_text? 'to find anywhere'
   end
 
-  it "knows whether we are editable or not" do
+  it 'knows whether we are editable or not' do
     placeholder.should_receive(:frankly_map).with(anything, 'isEnabled').and_return([true])
     placeholder.should be_enabled
   end
--- spec/lib/furter/accessors_spec.rb
+++ spec/lib/furter/accessors_spec.rb
@@ -9,29 +9,29 @@ end
 describe Furter::Accessors do
   let(:screen) { SomePage.new }
 
-  context("text") do
+  context('text') do
     let(:text) { double('Text accessor') }
 
     before(:each) do
       Furter::Accessors::Text.should_receive(:new).with(:placeholder => 'text').and_return(text)
     end
 
-    it "can be returned" do
+    it 'can be returned' do
       text.should_receive(:get_text)
       screen.text_field
     end
 
-    it "can be set" do
+    it 'can be set' do
       text.should_receive(:set_text).with('new text')
       screen.text_field = 'new text'
     end
 
-    it "has editability" do
+    it 'has editability' do
       text.should_receive(:editable?).and_return(false)
       screen.should_not be_text_field_editable
     end
 
-    it "can dish out a view" do
+    it 'can dish out a view' do
       screen.text_field_view.should be(text)
     end
   end
