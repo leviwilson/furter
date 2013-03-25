e0fb53ad15eddd0c9d14b6bf2e163b23c99fec46
Levi Wilson <levi@leviwilson.com>
2013-03-25 10:12:04 -0400

    moar specs around the base View

--- spec/lib/furter/accessors/view_spec.rb
+++ spec/lib/furter/accessors/view_spec.rb
@@ -1,6 +1,9 @@
 require 'spec_helper'
 
 describe Furter::Accessors::View do
+  let(:view) { Furter::Accessors::View.new(:label => 'id') }
+  let(:selector) { view.send(:selector) }
+
   context 'locating views' do
     it 'can be found by accessibility label' do
       by_label = Furter::Accessors::View.new(:label => 'id')
@@ -16,5 +19,20 @@ describe Furter::Accessors::View do
       with_extras = Furter::Accessors::View.new(:label => 'id', :extra => 'label')
       with_extras.send(:selector).should eq("view marked:'id' label")
     end
+  end
+
+  it 'can be clicked' do
+    view.should_receive(:wait_for_and_touch).with(selector)
+    view.click
+  end
+
+  it 'knows if it is visible' do
+    view.should_receive(:element_is_not_hidden).with(selector).and_return(true)
+    view.should be_visible
+  end
+
+  it 'knows if it is enabled' do
+    view.should_receive(:frankly_map).with(selector, 'isEnabled').and_return([true])
+    view.should be_enabled
   end
 end
