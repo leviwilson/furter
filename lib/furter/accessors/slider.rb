223d2649a82a63a007c596fcac70fac53728b70a
jeremy stewart <jlstewart379@gmail.com>
2013-04-11 09:11:02 -0400

    Some refactoring of furter

--- lib/furter/accessors/slider.rb
+++ lib/furter/accessors/slider.rb
@@ -1,12 +1,6 @@
 module Furter
   module Accessors
     class Slider < View
-      include Furter, Frank::Cucumber::FrankHelper, Frank::Cucumber::WaitHelper
-
-      def initialize(locator)
-        locator = locator.merge({:label => locator[:text]}) if locator[:text]
-        super(locator)
-      end
 
       def move(value)
         frankly_map(selector, 'setValue:animated:', value, 'YES')
@@ -16,8 +10,9 @@ module Furter
         frankly_map(selector, 'value')[0]
       end
 
-      def selector
-        "view:'UISlider' marked:'#{@locator[:label]}'"
+      def view_class
+        return "view:'#{@locator[:type]}'" if @locator[:type]
+        "view:'UISlider'"
       end
 
       def max_min(time)
