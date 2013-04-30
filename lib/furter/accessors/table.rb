80ffe894f0fea119341e512d378f715d6d658fcb
JeremyStewart <jlstewart379@gmail.com>
2013-04-29 20:57:03 -0400

    can count rows in table

--- lib/furter/accessors/table.rb
+++ lib/furter/accessors/table.rb
@@ -9,6 +9,16 @@ module Furter
         touch("#{selector} tableViewCell index:#{index}")
       end
 
+      def row_count
+        sections = frankly_map("#{selector}", 'numberOfSections')[0]
+        current_section = 1
+        until current_section > sections
+          rows = rows.to_i + frankly_map("#{selector}", 'numberOfRowsInSection:', "#{current_section}")
+          current_section += 1
+        end
+        rows
+      end
+
       private
       def selector
         "view:'UITableView' marked:'#{@locator[:label]}'"
