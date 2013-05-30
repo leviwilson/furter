33d60d73162a2be5e99623b997d8436f70e1f2e6
Jeremy Stewart <jlstewart379@gmail.com>
2013-05-30 07:26:30 -0400

    Added second spec for passing in path for screenshot

--- spec/lib/furter/furter_spec.rb
+++ spec/lib/furter/furter_spec.rb
@@ -7,6 +7,11 @@ describe Furter do
       Furter.should_receive(:frankly_screenshot).with('./screenshots/the_title.jpg', nil, true)
       Furter.screenshot('the_title')
     end
+
+    it 'can save a screen shot to a given path' do
+      Furter.should_receive(:frankly_screenshot).with('./path/the_title.jpg', nil, true)
+      Furter.screenshot('the_title', 'path')
+    end
   end
 end
 
